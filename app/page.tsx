b9e48aa746d4c8e9a841812ccd1042e98aa44f90
hoprik <42morozov42@gmail.com>
2024-06-02 17:37:09 +0300

    0.0.3

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
 export default function Home() {
-  return <><a href="/md">MD</a><br/><a href="/socket">СТРИМИНГ</a><br/><a href="/qrAuth">qr</a></>;
+  return <><a href="/md">MD</a><br/><a href="/socket">СТРИМИНГ</a><br/><a href="/qrAuth">qr авторизация</a><br/><a
+      href="/qrAuth">qr Скан</a></>;
 }
