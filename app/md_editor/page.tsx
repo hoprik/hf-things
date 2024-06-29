2e378f3cd4e010491afc6a1cb0943ddab2652d21
hoprik <42morozov42@gmail.com>
2024-06-29 17:25:59 +0300

    добавил редактор md файлов

---
+++ app/md_editor/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+import {config, MdEditor, ToolbarNames} from "md-editor-rt";
+import {useState} from "react";
+import { promises as fs } from 'fs';
+import 'md-editor-rt/lib/style.css';
+import RU from '@vavt/cm-extension/dist/locale/ru';
+
+config({
+    editorConfig: {
+        languageUserDefined: {
+            'ru': RU
+        }
+    }
+});
+
+const toolbar: ToolbarNames[] = [
+    '=',
+    'pageFullscreen',
+    'fullscreen',
+    'preview',
+    'previewOnly',
+    'htmlPreview',
+    'catalog',
+    'github'
+];
+
+export default function Page() {
+    const [mdText, setMdText] = useState<string>('# Привет редактор!');
+    const [subject, setSubject] = useState<string>("")
+    const [theme, setTheme] = useState<string>("")
+    const onSave = async (e: any)=>{
+        const saveData = {
+            mdText,
+            subject,
+            theme
+        }
+        const res = await fetch("/md_editor/api", {
+            method: "POST",
+            body: JSON.stringify(saveData)
+        })
+        const json = await res.json()
+        console.log(json)
+    }
+    return <>
+        <div style={{display: "flex", height:"50px"}}>
+            <input placeholder="Предмет" onChange={e => setSubject(e.target.value)}/>
+            <p>/</p>
+            <input placeholder="Тема" onChange={e => setTheme(e.target.value)}/>
+        </div>
+        <MdEditor modelValue={mdText} onChange={setMdText} language="ru" codeTheme="github" toolbarsExclude={toolbar}
+                  noUploadImg={true} autoDetectCode={true} onSave={onSave}/>
+    </>;
+}
